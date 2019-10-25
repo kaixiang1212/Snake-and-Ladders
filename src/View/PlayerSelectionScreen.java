95120eca0b4463fc536d52752f520ec023ff6c76
Kai Xiang Yong <z5175681@ad.unsw.edu.au>
2019-10-25 20:12:02 +1100

    Added player selection screen

---
+++ src/View/PlayerSelectionScreen.java
@@ -0,0 +1,37 @@
+package View;
+
+import Controller.PlayerSelectionController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PlayerSelectionScreen {
+
+    private Stage stage;
+    private String screenTitle;
+    private FXMLLoader fxmlLoader;
+
+    public PlayerSelectionScreen(Stage stage) {
+        this.stage = stage;
+        this.screenTitle = "Player Selection";
+        this.fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("View/PlayerSelection.fxml"));
+    }
+
+    public void start() {
+        stage.setTitle(screenTitle);
+        fxmlLoader.setController(new PlayerSelectionController(stage));
+        try {
+            Parent root = fxmlLoader.load();
+            Scene sc = new Scene(root);
+            stage.setScene(sc);
+            stage.show();
+            sc.getRoot().requestFocus();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
