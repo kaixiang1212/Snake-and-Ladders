d2cd6edf66380ff2e4734bfca23bd7ae79cb0882
Abdullah Wosaibi <z5104633@ad.unsw.edu.au>
2019-11-05 22:18:57 +1100

    Split animation code into own AnimationController class
    This class handles both dice and player animations

---
+++ src/Controller/AnimationController.java
@@ -0,0 +1,87 @@
+package Controller;
+
+import Model.GameEngine;
+import javafx.animation.AnimationTimer;
+
+public class AnimationController {
+	private final int maxFrames = 600;
+	private final int animationFrames = 10;
+	
+    private int frame;
+    private boolean isSpinning;
+    private boolean isPlayerMoving;
+    
+    private GameEngine engine;
+    private DiceController diceController;
+    
+    public AnimationController(GameEngine engine, DiceController diceController) {
+    	isSpinning = false;
+    	isPlayerMoving = false;
+    	frame = 0;
+    	this.engine = engine;
+    	this.diceController = diceController;
+    }
+    
+    public void setEngine(GameEngine engine) {
+    	this.engine = engine;
+    }
+    
+    /**
+     * Randomise Dice face for maxFrames frames
+     * after maxFrames frames it automatically stops
+     * 
+     * When a player is moving, move their token one position every animationFrames frames
+     * once the player reaches their destination, update the game state and prepare for next roll
+     */
+    private AnimationTimer animation = new AnimationTimer() {
+        public void handle(long time) {
+        	int currentPos = diceController.getCurrentPos();
+        	int destination = diceController.getDestination();
+        	
+        	if (isSpinning) {
+        		int diceFrame = (int) (Math.random() * 6) + 1;
+        		diceController.draw(diceFrame);
+            	frame++;
+            	if(frame == maxFrames) {
+            		diceController.stopButtonClicked();
+            	}
+        	} else if (isPlayerMoving) {		
+        		if (frame%animationFrames == 0 && currentPos == destination) {     			
+        			engine.updateState();
+        			diceController.idle();
+        		} else if (frame%animationFrames == 0 && currentPos <= destination) {
+        			engine.updatePosition(engine.getCurrentPlayer(), currentPos + 1);
+        		}
+        		frame++;	
+        	}
+        }
+    };
+    
+    /**
+     * Return the AnimationTimer object of the class
+     * @return animation: AnimationTimer object
+     */
+    public AnimationTimer getAnimation() {
+    	return animation;
+    }
+    
+    /**
+     * Sets whether the dice should be currently spinning or not
+     * @param value: true if dice should currently be spinning
+     * 				 false otherwise
+     */
+    public void setSpinning(boolean value) {
+    	frame = 0;
+    	isSpinning = value;
+    }
+    
+    /**
+     * Sets whether current player should be moving or not currently
+     * @param value: true if current player should currently be moving
+     * 			     false otherwise
+     */
+    public void setPlayerMoving(boolean value) {
+    	frame = 0;
+    	isPlayerMoving = value;
+    }
+}
--- src/Controller/DiceController.java
+++ src/Controller/DiceController.java
@@ -3,7 +3,6 @@ package Controller;
 import Model.Dice;
 import Model.GameEngine;
 import Model.Player;
-import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -27,13 +26,16 @@ public class DiceController {
     private ImageView playerToken;
 
     private GameEngine players;
+    private MusicController musicController;
+    private AnimationController animationController;
+    
     private Dice dice;
     private final Image[] diceFace;
-    private boolean isSpinning;
-    private boolean playerIsMoving;
-
-    private MusicController musicController;
-
+    private int currentPos;
+    private int destination;
+    private int dieRolled;
+    
+    
     public DiceController() {
         this.diceFace = new Image[6];
         diceFace[0] = new Image(String.valueOf(getClass().getClassLoader().getResource("asset/dice1.png")));
@@ -45,62 +47,116 @@ public class DiceController {
         musicController = new MusicController();
         musicController.initDice();
         dice = new Dice();
-        animation.start();
-        isSpinning = false;
-        playerIsMoving = false;
+        animationController = new AnimationController(players, this);
+        animationController.getAnimation().start();
     }
 
     /**
      * Configuration for Dice Controller
-     * called by Board Controller to communicate with board
+     * called by BoardController to communicate with board
      *
      * @param engine Game Engine
      */
     void config(GameEngine engine) {
         this.players = engine;
+        animationController.setEngine(engine);
         setCurrentPlayerToken();
         Player player = players.getCurrentPlayer();
         StringBuilder sb = new StringBuilder();
-        message.setText(sb.append("\n").append(player.getPlayerName()).append("'s turn:\n").toString());
+        message.setText((sb.append("\n").append(player.getPlayerName()).append("'s turn:\n").toString()));
     }
-
+    
+    /**
+     * Called when the 'roll' button is clicked
+     */
     @FXML
     private void rollButtonClicked() {
         musicController.playRollDice();
         text.setText("");
-        //animation.start();
-        isSpinning = true;
+        animationController.setSpinning(true);
         button.setText("Stop");
         button.setOnAction(event -> stopButtonClicked());
         diceImage.setOnMouseClicked(mouseEvent -> stopButtonClicked());
     }
-
+    
+    /**
+     * Called when the 'stop' button is clicked
+     */
     @FXML
-    private void stopButtonClicked() {
+	public void stopButtonClicked() {
         musicController.clear();
-        //animation.stop();
-        isSpinning = false;
+        animationController.setSpinning(false);
         button.setDisable(true);
-        diceImage.setDisable(true);
-        
+        diceImage.setDisable(true);   
 
         Player currentPlayer = players.getCurrentPlayer();
         int diceResult = dice.roll();
         text.setText(currentPlayer.getPlayerName() + " rolled " + diceResult);
         musicController.playThrowDice();
         draw(diceResult);
-
-    	if(!players.isFinished()) {
-    		
-            currentPos = players.getBoard().getPosition(currentPlayer.getX(), currentPlayer.getY());
-            destination = currentPos+diceResult;
-            playerToMove = currentPlayer; 
-            dieRolled = diceResult;
-            playerIsMoving = true;
-            
-    	}
-
         
+    	if(!players.isFinished()) {
+            currentPos = getCurrentPos();
+            destination = currentPos+diceResult;
+            destination = getDestination();
+            dieRolled = diceResult;
+            
+            animationController.setPlayerMoving(true);
+    	}
+        
+    }
+    
+    /**
+     * Called in-between player turns to prepare for the next player roll
+     */
+    public void idle() {
+    	animationController.setSpinning(false);
+    	animationController.setPlayerMoving(false);
+    	StringBuilder sb = new StringBuilder();
+    	if (players.isFinished()) {
+            sb.append(players.getCurrentPlayer().getPlayerName()).append(" has won the game! Congratulations!\n");
+            message.setText((sb.toString()));
+            return;
+        }
+    	
+    	button.setDisable(false);
+        diceImage.setDisable(false);
+
+    	if (dieRolled == 6) {
+            musicController.playRolled6();
+            sb.append(players.getCurrentPlayer().getPlayerName().concat(" roll again"));
+        } else {
+        	players.nextPlayer();
+        }
+    	
+    	sb.append("\n");
+    	sb.append(players.getCurrentPlayer().getPlayerName()).append("'s turn:\n");
+    	message.setText(sb.toString());
+    	
+        button.setText("Start Rolling");
+        diceImage.setOnMouseClicked(mouseEvent -> rollButtonClicked());
+        button.setOnAction(event -> rollButtonClicked());
+        players.clearConsole();
+        setCurrentPlayerToken();
+    }
+    
+    /**
+     * Returns the current player's current position on the board
+     * @return current player's current board position
+     */
+    public int getCurrentPos() {
+    	Player currentPlayer = players.getCurrentPlayer();
+    	return players.getBoard().getPosition(currentPlayer.getX(), currentPlayer.getY());
+    }
+    
+    /**
+     * Returns the destined position of the current player on the board
+     * @return destination of current player
+     */
+    public int getDestination() {
+    	destination = Math.max(destination, players.getBoard().getMinPos());
+        destination = Math.min(destination, players.getBoard().getMaxPos());
+    	return destination;
     }
 
     /**
@@ -108,77 +164,14 @@ public class DiceController {
      *
      * @param dieFace Number to be drawn
      */
-    private void draw(int dieFace) {
+    public void draw(int dieFace) {
         Image image = this.diceFace[dieFace - 1];
         diceImage.setImage(image);
     }
-
-    //private final int maxFrame = 1000;
-    private int frame = 0;
-    private int currentPos;
-    private int destination;
-    private Player playerToMove;
-    private int dieRolled;
-
+    
     /**
-     * Randomise Dice face for 1000 frames
-     * after 1000 frames it automatically stops
+     * Updates the current player's token to the correct player
      */
-    private AnimationTimer animation = new AnimationTimer() {
-        public void handle(long time) {
-        	if (isSpinning) {
-        		int diceFrame = (int) (Math.random() * 6) + 1;
-        		draw(diceFrame);
-            	frame++;
-            	
-        	} else if (playerIsMoving) {
-        		
-        		if (currentPos == destination) {
-        			
-        	        StringBuilder sb = new StringBuilder();
-        			
-        			if (players.isFinished()) {
-        	            sb.append(playerToMove.getPlayerName()).append(" has won the game! Congratulations!\n");
-        	            message.setText(sb.toString());
-        	            return;
-        	        }
-
-        	        if (dieRolled == 6) {
-        	            musicController.playRolled6();
-        	            sb.append(playerToMove.getPlayerName()).append(" roll again\n");
-        	        } else {
-        	            sb.append("\n");
-        	            playerToMove = players.nextPlayer();
-        	        }
-
-        	        sb.append(playerToMove.getPlayerName()).append("'s turn:\n");
-
-        	        button.setDisable(false);
-        	        diceImage.setDisable(false);
-        	        button.setText("Start Rolling");
-        	        diceImage.setOnMouseClicked(mouseEvent -> rollButtonClicked());
-        	        button.setOnAction(event -> rollButtonClicked());
-
-        	        message.setText(sb.toString());
-        	        players.clearConsole();
-        	        setCurrentPlayerToken();
-        			players.updateState();
-        			playerIsMoving = false;
-        			
-        		} else if (frame >= 8 && currentPos <= destination) {
-        			
-        			players.updatePosition(playerToMove, currentPos + 1);
-        			currentPos++;
-        			frame = 0;
-
-        		}
-        		draw(dieRolled);
-        		frame++;
-        		
-        	}
-        }
-    };
-
     private void setCurrentPlayerToken() {
         int token = players.getCurrentPlayerToken();
         playerToken.setImage(new Image(String.valueOf(getClass().getClassLoader().getResource("asset/token" + token + ".png"))));
