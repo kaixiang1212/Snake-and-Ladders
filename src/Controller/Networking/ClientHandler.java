4b3df1e16ca16f8888e0d809f3a8345c0e0aa992
Kai Xiang <z5175681@ad.unsw.edu.au>
2019-11-11 18:35:50 +1100

    added simple server using WebSocket protocol

---
+++ src/Controller/Networking/ClientHandler.java
@@ -0,0 +1,73 @@
+package Controller.Networking;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler extends Thread {
+
+    private final Socket clientSocket;
+    private final Server server;
+    private OutputStream outputStream;
+    private int player;
+
+    ClientHandler(Server server, int player, Socket clientSocket) throws IOException {
+        this.player = player;
+        this.server = server;
+        this.clientSocket = clientSocket;
+        outputStream = clientSocket.getOutputStream();
+    }
+
+    @Override
+    public void run() {
+        try {
+            handleClientSocket();
+        } catch (IOException e) {
+            try {
+                closeSocket();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void handleClientSocket() throws IOException {
+        InputStream inputStream = clientSocket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String msg = "You are player " + player + "\n";
+        msg = msg + "Current players " + server.getNumClient() + "/" + server.getNumPlayer() + "\n";
+        send(msg);
+
+        String line;
+        while ( (line = reader.readLine()) != null ){
+            String[] token = line.split(" ");
+            String cmd = token[0];
+            if ("quit".equalsIgnoreCase(cmd)|| "exit".equals(cmd)) break;
+            else if ("roll".equalsIgnoreCase(cmd)){
+                handleRoll();
+            } else {
+                if ("".equals(cmd)) continue;
+                msg = "Unknown command: " + cmd + "\n";
+                send(msg);
+            }
+        }
+        closeSocket();
+    }
+
+    private void handleRoll() {
+    }
+
+    void send(String msg) throws IOException {
+        outputStream.write(msg.getBytes());
+    }
+
+    private void closeSocket() throws IOException {
+        server.removePlayer(this);
+        clientSocket.close();
+    }
+
+    int getPlayerNo(){
+        return player;
+    }
+
+}
---
+++ src/Controller/Networking/Server.java
@@ -0,0 +1,85 @@
+package Controller.Networking;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+
+    private ArrayList<ClientHandler> clientList;
+    private int numPlayer;
+    private int[] player;
+
+    private Server(int numPlayer){
+        clientList = new ArrayList<>();
+        this.numPlayer = numPlayer;
+        this.player = new int[numPlayer];
+    }
+
+    List<ClientHandler> getClientList(){
+        return clientList;
+    }
+
+    int getNumClient(){
+        return clientList.size();
+    }
+
+    int getNumPlayer() {
+        return numPlayer;
+    }
+
+    private int getNextSlot(){
+        for (int i=0;i < numPlayer;i++){
+            if (player[i] != 1) return i + 1;
+        }
+        return -1;
+    }
+
+    private void addPlayer(int slot, Socket clientSocket) throws IOException {
+        ClientHandler clientHandler = new ClientHandler(this, slot, clientSocket);
+        player[slot-1] = 1;
+        for (ClientHandler client : clientList){
+            String msg = "Player " + slot + " joined the game\n";
+            client.send(msg);
+        }
+        clientList.add(clientHandler);
+        clientHandler.start();
+    }
+
+    void removePlayer(ClientHandler clientHandler) throws IOException {
+        int i = clientHandler.getPlayerNo();
+        player[i-1] = 0;
+        clientList.remove(clientHandler);
+        for (ClientHandler client : clientList){
+            String msg = "Player " + i + " left the game\n";
+            client.send(msg);
+        }
+    }
+
+    public void start() {
+        int port = 8888;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                int slot = getNextSlot();
+                if (slot != -1) {
+                    addPlayer(slot, clientSocket);
+                } else {
+                    clientSocket.getOutputStream().write("Maximum player exceeded\n".getBytes());
+                    clientSocket.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(3);
+        server.start();
+    }
+
+}
